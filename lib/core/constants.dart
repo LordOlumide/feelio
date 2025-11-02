fa488f59976cb09786a0d1b10c47ca351b6d2495
Aderemi Alo <aloaderemi1@gmail.com>
2025-11-02 19:45:06 +0100

    Add AppColors class with a range of color constants for the application

---
+++ lib/core/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const happy1 = Color(0xFFFFF085);
+  static const happy2 = Color(0xFFFFD6A7);
+  static const happy3 = Color(0xFFFFD740);
+  static const happy4 = Color(0xFFFFC400);
+  static const happy5 = Color(0xFFFFB700);
+  static const happy6 = Color(0xFFFFA000);
+  static const happy7 = Color(0xFFFF8F00);
+  static const happy8 = Color(0xFFFF6F00);
+  static const happy9 = Color(0xFFFF5200);
+  static const happy10 = Color(0xFFFF3D00);
+}
